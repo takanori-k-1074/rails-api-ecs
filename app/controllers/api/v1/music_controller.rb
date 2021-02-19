a953aa189397a1181b00aa67a82cc79cd8c4884c
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2021-02-19 19:51:34 +0900

    music controller

---
+++ app/controllers/api/v1/music_controller.rb
@@ -0,0 +1,50 @@
+class Api::V1::MusicController < ApplicationController
+  protect_from_forgery except: [:create, :update, :destroy]
+
+  def index
+    @musics = Music.all
+    render 'index', formats: :json, handlers: 'jbuilder'
+  end
+
+  def show
+    @music = Music.find(params[:id])
+    render "show", formats: :json, handlers: "jbuilder"
+  end
+
+  def create
+    @music = Music.new(music_params)
+    if @music.save
+      head :no_content
+    else
+      render json: @music.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @music = Music.find(params[:id])
+    if @music.update_attributes(music_params)
+      render 'index', formats: :json, handlers: 'jbuilder'
+    else
+      render json: @music.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @music = Music.find(params[:id])
+    if @music.destroy
+      head :no_content
+    else
+      render json: @music.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def music_params
+    params.fetch(:music, {}).permit(
+      :id, :title, :artist, :genre
+    )
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :weather, only: [:index]
+      resources :music
     end
   end
 end
